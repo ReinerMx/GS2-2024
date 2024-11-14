a2e1ae2aa0d27ce073f5f2a953fd0c2d61405e67
Kubisili <jakub189alex@gmail.com>
2024-11-14 22:39:07 +0100

    Role entfernt

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,6 @@ const User = sequelize.define('User', {
   username: { type: DataTypes.STRING, allowNull: false, unique: true },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM('admin', 'user'), defaultValue: 'user' },
 }, {
   hooks: {
     beforeCreate: async (user) => {
