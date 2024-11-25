ac465a69ebcf7b056fe3bc963201f0f3bfd0fee2
Amelie Luschnig <Amelie@MacBook-Pro-105.fritz.box>
2024-11-25 22:57:44 +0100

    Docker mit pgstac

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config.json');
 
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  {
-    host: config.development.host,
-    port: config.development.port,
-    dialect: config.development.dialect,
+const sequelize = new Sequelize('gs2_database', 'postgres', 'mysecretpassword', {
+  host: 'pgstac',
+  dialect: 'postgres',
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
   }
-);
+})();
+
 
 module.exports = sequelize;
